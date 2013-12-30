82ef19abf72e32f883c3cec3f77de14d5e62c317
Simon <almbro89@gmail.com>
2013-12-30 07:17:38 +0100

    Added simple RGB filters as an example

--- Textifier/src/main/java/com/example/textifier/ResultActivity.java
+++ Textifier/src/main/java/com/example/textifier/ResultActivity.java
@@ -1,11 +1,18 @@
 package com.example.textifier;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 /**
  * Created by simon on 12/30/13.
@@ -13,28 +20,75 @@ import android.widget.Toast;
  */
 public class ResultActivity extends Activity {
 
+    ImageView imageView;
+
+    boolean r,g,b;
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        Bundle b = getIntent().getExtras();
+        Bundle bundle = getIntent().getExtras();
 
-        if(b != null && b.containsKey("IMAGE_BYTE_ARRAY")){
-            byte[] image = b.getByteArray("IMAGE_BYTE_ARRAY");
+        r = false;
+        g = false;
+        b = false;
+
+        if(bundle != null && bundle.containsKey("IMAGE_BYTE_ARRAY")){
+            byte[] image = bundle.getByteArray("IMAGE_BYTE_ARRAY");
 
             setContentView(R.layout.result_layout);
 
             Bitmap bmp = BitmapFactory.decodeByteArray(image, 0, image.length);
 
 
-            ImageView iV = (ImageView)findViewById(R.id.resultImageView);
+            imageView = (ImageView)findViewById(R.id.resultImageView);
 
-            iV.setImageBitmap(bmp);
-            iV.setRotation((float)90.0);
+            imageView.setImageBitmap(bmp);
+            imageView.setRotation((float)90.0);
         }
         else {
             Toast.makeText(getApplicationContext(), "Did not contain image byte array", Toast.LENGTH_SHORT).show();
         }
     }
 
+    private void applyColorFilter(ImageView iv, int color){
+
+        iv.setColorFilter(color, PorterDuff.Mode.ADD);
+
+    }
+
+    public void onRedClicked(View view){
+        ToggleButton tb = (ToggleButton) view;
+        r = tb.isChecked();
+        updateColorFilter();
+    }
+
+    public void onGreenClicked(View view){
+        ToggleButton tb = (ToggleButton) view;
+        g = tb.isChecked();
+        updateColorFilter();
+    }
+
+    public void onBlueClicked(View view){
+        ToggleButton tb = (ToggleButton) view;
+        b = tb.isChecked();
+        updateColorFilter();
+    }
+
+    private void updateColorFilter(){
+        imageView.clearColorFilter();
+        int color = 0;
+        if(r){
+            color += Color.RED;
+        }
+        if(g){
+            color += Color.GREEN;
+        }
+        if(b){
+            color += Color.BLUE;
+        }
+        applyColorFilter(imageView, color);
+    }
 }
